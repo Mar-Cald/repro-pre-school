62c512b0f3a917b808e96aaf6a0a80ab92696968
Mar-Cald <margherita.calderan@gmail.com>
2025-06-04 10:00:27 +0200

    add quarto example

---
+++ slide/R/utils.R
@@ -0,0 +1,11 @@
+simulate_data = function(n,eff){
+  
+  x <- rep(c(0, 1),each = n) # 2 conditions
+  
+  y <- 0 + x * eff + rnorm(n*2,0,1)
+  
+  # Combina i dati in un dataframe
+  return(data.frame(
+    x = ifelse(x == 0, "control","experimental"),
+    y = y))
+}
